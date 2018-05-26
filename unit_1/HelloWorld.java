4ba5e17f942b13e0d3b06a2984ef1d55cdcb91a1
conted <conted@BH210-02.studentservers.JAC-SERVERS.JOHNABBOTT.QC.CA>
2018-05-26 13:37:42 -0400

    My Lecture 1 notes Day1

---
+++ unit_1/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+    }
+    public void printSum(int x, int y)
+    {
+        System.out.println(x+y);
+    }
+
+}
